ae22257ac4a5ed92619271780f015d64c27143c4
Manuel Raimann <raimannma@outlook.de>
2019-03-10 12:00:15 +0100

    cleanup

--- src/main/java/com/github/chen0040/rl/learning/rlearn/RAgent.java
+++ src/main/java/com/github/chen0040/rl/learning/rlearn/RAgent.java
@@ -3,99 +3,93 @@ package com.github.chen0040.rl.learning.rlearn;
 import com.github.chen0040.rl.utils.IndexValue;
 
 import java.io.Serializable;
-import java.util.Random;
 import java.util.Set;
 
 
 /**
  * Created by xschen on 9/27/2015 0027.
  */
-public class RAgent implements Serializable{
+public class RAgent implements Serializable {
     private RLearner learner;
     private int currentState;
     private int currentAction;
     private double currentValue;
 
-    public int getCurrentState(){
-        return currentState;
+    public RAgent() {
+
     }
 
-    public int getCurrentAction(){
-        return currentAction;
+    public RAgent(final int stateCount, final int actionCount, final double alpha, final double beta, final double rho, final double initialQ) {
+        this.learner = new RLearner(stateCount, actionCount, alpha, beta, rho, initialQ);
     }
 
-    public void start(int currentState){
+    public RAgent(final int stateCount, final int actionCount) {
+        this.learner = new RLearner(stateCount, actionCount);
+    }
+
+    public int getCurrentState() {
+        return this.currentState;
+    }
+
+    public int getCurrentAction() {
+        return this.currentAction;
+    }
+
+    public void start(final int currentState) {
         this.currentState = currentState;
     }
 
-    public RAgent makeCopy(){
-        RAgent clone = new RAgent();
+    public RAgent makeCopy() {
+        final RAgent clone = new RAgent();
         clone.copy(this);
         return clone;
     }
 
-    public void copy(RAgent rhs){
-        currentState = rhs.currentState;
-        currentAction = rhs.currentAction;
-        learner.copy(rhs.learner);
+    public void copy(final RAgent rhs) {
+        this.currentState = rhs.currentState;
+        this.currentAction = rhs.currentAction;
+        this.learner.copy(rhs.learner);
     }
 
     @Override
-    public boolean equals(Object obj){
-        if(obj != null && obj instanceof RAgent){
-            RAgent rhs = (RAgent)obj;
-            if(!learner.equals(rhs.learner)) return false;
-            if(currentAction != rhs.currentAction) return false;
-            return currentState == rhs.currentState;
+    public boolean equals(final Object obj) {
+        if (obj instanceof RAgent) {
+            final RAgent rhs = (RAgent) obj;
+            return this.learner.equals(rhs.learner) && this.currentAction == rhs.currentAction && this.currentState == rhs.currentState;
         }
         return false;
     }
 
-    public IndexValue selectAction(){
-        return selectAction(null);
+    public IndexValue selectAction() {
+        return this.selectAction(null);
     }
 
-    public IndexValue selectAction(Set<Integer> actionsAtState){
-
-        if(currentAction==-1){
-            IndexValue iv = learner.selectAction(currentState, actionsAtState);
-            currentAction = iv.getIndex();
-            currentValue = iv.getValue();
+    public IndexValue selectAction(final Set<Integer> actionsAtState) {
+        if (this.currentAction == -1) {
+            final IndexValue iv = this.learner.selectAction(this.currentState, actionsAtState);
+            this.currentAction = iv.getIndex();
+            this.currentValue = iv.getValue();
         }
-        return new IndexValue(currentAction, currentValue);
+        return new IndexValue(this.currentAction, this.currentValue);
     }
 
-    public void update(int newState, double immediateReward){
-        update(newState, null, immediateReward);
+    public void update(final int newState, final double immediateReward) {
+        this.update(newState, null, immediateReward);
     }
 
-    public void update(int newState, Set<Integer> actionsAtState, double immediateReward){
-        if(currentAction != -1) {
-            learner.update(currentState, currentAction, newState, actionsAtState, immediateReward);
-            currentState = newState;
-            currentAction = -1;
+    public void update(final int newState, final Set<Integer> actionsAtState, final double immediateReward) {
+        if (this.currentAction != -1) {
+            this.learner.update(this.currentState, this.currentAction, newState, actionsAtState, immediateReward);
+            this.currentState = newState;
+            this.currentAction = -1;
         }
     }
 
-    public RAgent(){
-
+    public RLearner getLearner() {
+        return this.learner;
     }
 
-
-
-    public RLearner getLearner(){
-        return learner;
-    }
-
-    public void setLearner(RLearner learner){
+    public void setLearner(final RLearner learner) {
         this.learner = learner;
-    }
-
-    public RAgent(int stateCount, int actionCount, double alpha, double beta, double rho, double initialQ){
-        learner = new RLearner(stateCount, actionCount, alpha, beta, rho, initialQ);
-    }
-
-    public RAgent(int stateCount, int actionCount){
-        learner = new RLearner(stateCount, actionCount);
     }
 }
--- src/main/java/com/github/chen0040/rl/utils/Vec.java
+++ src/main/java/com/github/chen0040/rl/utils/Vec.java
@@ -2,16 +2,16 @@ package com.github.chen0040.rl.utils;
 
 import java.io.Serializable;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.IntStream;
 
 
 /**
  * Created by xschen on 9/27/2015 0027.
  */
 public class Vec implements Serializable {
-    private Map<Integer, Double> data = new HashMap<Integer, Double>();
+    private final Map<Integer, Double> data = new HashMap<Integer, Double>();
     private int dimension;
     private double defaultValue;
     private int id = -1;
@@ -52,9 +52,7 @@ public class Vec implements Serializable {
         this.id = rhs.id;
 
         this.data.clear();
-        for (final Map.Entry<Integer, Double> entry : rhs.data.entrySet()) {
-            this.data.put(entry.getKey(), entry.getValue());
-        }
+        rhs.data.forEach(this.data::put);
     }
 
     public void set(final int i, final double value) {
@@ -75,33 +73,19 @@ public class Vec implements Serializable {
 
     @Override
     public boolean equals(final Object rhs) {
-        if (rhs != null && rhs instanceof Vec) {
+        if (rhs instanceof Vec) {
             final Vec rhs2 = (Vec) rhs;
-            if (this.dimension != rhs2.dimension) {
+            if (this.dimension != rhs2.dimension || this.data.size() != rhs2.data.size()) {
                 return false;
             }
-
-            if (this.data.size() != rhs2.data.size()) {
-                return false;
-            }
-
             for (final Integer index : this.data.keySet()) {
-                if (!rhs2.data.containsKey(index)) {
-                    return false;
-                }
-                if (!DoubleUtils.equals(this.data.get(index), rhs2.data.get(index))) {
+                if (!rhs2.data.containsKey(index) || !DoubleUtils.equals(this.data.get(index), rhs2.data.get(index))) {
                     return false;
                 }
             }
-
             if (this.defaultValue != rhs2.defaultValue) {
-                for (int i = 0; i < this.dimension; ++i) {
-                    if (this.data.containsKey(i)) {
-                        return false;
-                    }
-                }
+                return IntStream.range(0, this.dimension).noneMatch(this.data::containsKey);
             }
-
             return true;
         }
 
@@ -110,9 +94,7 @@ public class Vec implements Serializable {
 
     public void setAll(final double value) {
         this.defaultValue = value;
-        for (final Integer index : this.data.keySet()) {
-            this.data.put(index, this.defaultValue);
-        }
+        this.data.keySet().forEach(index -> this.data.put(index, this.defaultValue));
     }
 
     public IndexValue indexWithMaxValue(final Set<Integer> indices) {
@@ -133,7 +115,7 @@ public class Vec implements Serializable {
         }
     }
 
-    public IndexValue indexWithMaxValue() {
+    private IndexValue indexWithMaxValue() {
         final IndexValue iv = new IndexValue();
         iv.setIndex(-1);
         iv.setValue(Double.NEGATIVE_INFINITY);
@@ -169,46 +151,7 @@ public class Vec implements Serializable {
     }
 
 
-    public Vec projectOrthogonal(final Iterable<Vec> vlist) {
-        Vec b = this;
-        for (final Vec v : vlist) {
-            b = b.minus(b.projectAlong(v));
-        }
-
-        return b;
-    }
-
-    public Vec projectOrthogonal(final List<Vec> vlist, final Map<Integer, Double> alpha) {
-        Vec b = this;
-        for (int i = 0; i < vlist.size(); ++i) {
-            final Vec v = vlist.get(i);
-            final double norm_a = v.multiply(v);
-
-            if (DoubleUtils.isZero(norm_a)) {
-                return new Vec(this.dimension);
-            }
-            final double sigma = this.multiply(v) / norm_a;
-            final Vec v_parallel = v.multiply(sigma);
-
-            alpha.put(i, sigma);
-
-            b = b.minus(v_parallel);
-        }
-
-        return b;
-    }
-
-    public Vec projectAlong(final Vec rhs) {
-        final double norm_a = rhs.multiply(rhs);
-
-        if (DoubleUtils.isZero(norm_a)) {
-            return new Vec(this.dimension);
-        }
-        final double sigma = this.multiply(rhs) / norm_a;
-        return rhs.multiply(sigma);
-    }
-
-    public Vec multiply(final double rhs) {
+    private Vec multiply(final double rhs) {
         final Vec clone = this.makeCopy();
         for (final Integer i : this.data.keySet()) {
             clone.data.put(i, rhs * this.data.get(i));
@@ -216,86 +159,24 @@ public class Vec implements Serializable {
         return clone;
     }
 
-    public double multiply(final Vec rhs) {
-        double productSum = 0;
-        if (this.defaultValue == 0) {
-            for (final Map.Entry<Integer, Double> entry : this.data.entrySet()) {
-                productSum += entry.getValue() * rhs.get(entry.getKey());
-            }
-        } else {
-            for (int i = 0; i < this.dimension; ++i) {
-                productSum += this.get(i) * rhs.get(i);
-            }
-        }
+    double multiply(final Vec rhs) {
 
-        return productSum;
+        return this.defaultValue == 0 ?
+                this.data.entrySet().stream().mapToDouble(entry -> entry.getValue() * rhs.get(entry.getKey())).sum() :
+                IntStream.range(0, this.dimension).mapToDouble(i -> this.get(i) * rhs.get(i)).sum();
     }
 
-    public Vec pow(final double scalar) {
-        final Vec result = new Vec(this.dimension);
-        for (final Map.Entry<Integer, Double> entry : this.data.entrySet()) {
-            result.data.put(entry.getKey(), Math.pow(entry.getValue(), scalar));
-        }
-        return result;
+    private double sum() {
+        return this.data.values().stream().mapToDouble(v -> v).sum() + this.defaultValue * (this.dimension - this.data.size());
     }
 
-    public Vec add(final Vec rhs) {
-        final Vec result = new Vec(this.dimension);
-        int index;
-        for (final Map.Entry<Integer, Double> entry : this.data.entrySet()) {
-            index = entry.getKey();
-            result.data.put(index, entry.getValue() + rhs.data.get(index));
-        }
-        for (final Map.Entry<Integer, Double> entry : rhs.data.entrySet()) {
-            index = entry.getKey();
-            if (result.data.containsKey(index)) {
-                continue;
-            }
-            result.data.put(index, entry.getValue() + this.data.get(index));
-        }
-
-        return result;
-    }
-
-    public Vec minus(final Vec rhs) {
-        final Vec result = new Vec(this.dimension);
-        int index;
-        for (final Map.Entry<Integer, Double> entry : this.data.entrySet()) {
-            index = entry.getKey();
-            result.data.put(index, entry.getValue() - rhs.data.get(index));
-        }
-        for (final Map.Entry<Integer, Double> entry : rhs.data.entrySet()) {
-            index = entry.getKey();
-            if (result.data.containsKey(index)) {
-                continue;
-            }
-            result.data.put(index, this.data.get(index) - entry.getValue());
-        }
-
-        return result;
-    }
-
-    public double sum() {
-        double sum = 0;
-
-        for (final Map.Entry<Integer, Double> entry : this.data.entrySet()) {
-            sum += entry.getValue();
-        }
-        sum += this.defaultValue * (this.dimension - this.data.size());
-
-        return sum;
-    }
-
-    public boolean isZero() {
+    boolean isZero() {
         return DoubleUtils.isZero(this.sum());
     }
 
-    public double norm(final int level) {
+    double norm(final int level) {
         if (level == 1) {
-            double sum = 0;
-            for (final Double val : this.data.values()) {
-                sum += Math.abs(val);
-            }
+            double sum = this.data.values().stream().mapToDouble(Math::abs).sum();
             if (!DoubleUtils.isZero(this.defaultValue)) {
                 sum += Math.abs(this.defaultValue) * (this.dimension - this.data.size());
             }
@@ -307,10 +188,7 @@ public class Vec implements Serializable {
             }
             return Math.sqrt(sum);
         } else {
-            double sum = 0;
-            for (final Double val : this.data.values()) {
-                sum += Math.pow(Math.abs(val), level);
-            }
+            double sum = this.data.values().stream().mapToDouble(val -> Math.pow(Math.abs(val), level)).sum();
             if (!DoubleUtils.isZero(this.defaultValue)) {
                 sum += Math.pow(Math.abs(this.defaultValue), level) * (this.dimension - this.data.size());
             }
@@ -318,7 +196,7 @@ public class Vec implements Serializable {
         }
     }
 
-    public Vec normalize() {
+    Vec normalize() {
         final double norm = this.norm(2); // L2 norm is the cartesian distance
         if (DoubleUtils.isZero(norm)) {
             return new Vec(this.dimension);
@@ -326,41 +204,19 @@ public class Vec implements Serializable {
         final Vec clone = new Vec(this.dimension);
         clone.setAll(this.defaultValue / norm);
 
-        for (final Integer k : this.data.keySet()) {
-            clone.data.put(k, this.data.get(k) / norm);
-        }
+        this.data.keySet().forEach(k -> clone.data.put(k, this.data.get(k) / norm));
         return clone;
     }
 
-    public Map<Integer, Double> getData() {
+    Map<Integer, Double> getData() {
         return this.data;
     }
 
-    public void setData(final Map<Integer, Double> data) {
-        this.data = data;
-    }
-
-    public int getDimension() {
+    int getDimension() {
         return this.dimension;
     }
 
-    public void setDimension(final int dimension) {
-        this.dimension = dimension;
-    }
-
-    public double getDefaultValue() {
-        return this.defaultValue;
-    }
-
-    public void setDefaultValue(final double defaultValue) {
-        this.defaultValue = defaultValue;
-    }
-
-    public int getId() {
-        return this.id;
-    }
-
-    public void setId(final int id) {
+    void setId(final int id) {
         this.id = id;
     }
 }
